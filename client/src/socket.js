e5bc1b65649ea3059f316ba9515815f4797e71b7
manthan <manthan.jsharma@gmail.com>
2026-02-14 15:29:58 +0530

    add: render URL

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const URL =
   process.env.NODE_ENV === "production"
-    ? "https://pixelwars-backend.onrender.com"
+    ? "https://socket-5ezz.onrender.com"
     : "http://localhost:3001";
 
 export const socket = io(URL);
